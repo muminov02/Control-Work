6fd29a460f544d26126ce17fb301eaf3544a875b
Umid <spec7575@mail.ru>
2022-07-02 12:52:49 +0500

    Заполнил новый массив

--- final/Program.cs
+++ final/Program.cs
@@ -6,3 +6,7 @@ for (int i = 0; i < arr.Length; i++)
     if (arr[i].Length <= 3) count++;
 }
 string[] new_arr = new string[count];
+for (int j = 0; j < arr.Length; j++)
+{
+    if (arr[j].Length <= 3) { new_arr[count2] = arr[j]; count2++; }
+}
